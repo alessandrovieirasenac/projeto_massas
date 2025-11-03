045a42218a1c746116e9678b145f6f6b3bf0ba0d
ALESSANDRO DA COSTA VIEIRA <alessandro.vieira@educacao.senac.rio>
2025-11-03 19:25:19 -0300

    Atualizacao_05

--- massas/settings.py
+++ massas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+4+^l^d258$^w(=_$or2_2t&uoo+)$boybk5xgvsk2bim#t1kf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
